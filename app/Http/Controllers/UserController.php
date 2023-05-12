0cf5ecf60a6c6890ca376ce1349853465038d653
Bellagha Youssef <bellagha.youssef@exon-technologies.ca>
2023-05-12 10:17:53 +0100

    ta

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -62,7 +62,8 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+                $user = User::findOrFail($id);
+                return response()->json(['user' => $user], 200);
     }
 
     /**
